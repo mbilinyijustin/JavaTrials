f59bfe25f66eaaeef47144d9918ac8c9eb330abd
mbilinyijustin14@gmail.com <mbilinyijustin14@gmail.com>
2024-12-12 11:34:49 +0300

    mambo

---
+++ src/Main.kt
@@ -0,0 +1,34 @@
+//fun main(){
+////    println("Hello")
+////    println("Alice")
+////    val name = "vene"
+////    val age = 32
+////    var namee = "willy"
+////    namee = "mbilinyi"
+////    println(name)
+////    print(name)
+////    print(age)
+////    println(namee)
+////    println(namee)
+////    println("The age is $age")
+//
+//}
+//fun main(){
+//    val name :String = "computer"
+//    val age :Int = 34
+//    val wheight :Float = 345.987654333F
+//
+//}
+fun main(){
+//    sumCaluculator(4, 4)
+    println(sumCaluculator(9, 5))
+    println(sumCaluculator(9, 5))
+}
+
+
+fun sumCaluculator(num1:Int, num2:Int):Int{
+    val sum = num1+num2
+//      println("this is a non paramenter fun \n The sum is: $sum")
+    return sum
+
+}
